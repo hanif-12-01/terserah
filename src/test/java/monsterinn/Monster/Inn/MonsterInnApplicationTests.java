0cf25adb6d2b16a21caf53f9e17aa822453fedec
DZAKI KHOTHIR <dzaki.khothir123@gmail.com>
2026-04-21 21:13:33 +0700

    Setup Awal Arsitektur Folder Monster Inn

---
+++ src/test/java/monsterinn/Monster/Inn/MonsterInnApplicationTests.java
@@ -0,0 +1,13 @@
+package monsterinn.Monster.Inn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterInnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
